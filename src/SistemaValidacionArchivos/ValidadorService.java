2d11a06e1b4a86fb9ac9c63d5d01cbcbf8d2bb27
jhoshua jaimes <jhoshuajaimesnatera@gmail.com>
2026-04-14 20:43:54 +0200

    Otros

--- src/SistemaValidacionArchivos/ValidadorService.java
+++ src/SistemaValidacionArchivos/ValidadorService.java
@@ -9,11 +9,11 @@ public class ValidadorService {
 	
 	public boolean validarTodo() {
 		for(Validable v: fichero) {
-			if(v.isValid()) { //manera pro !v.isValid()
-				return true;
+			if(v.isValid()==false) { //manera pro !v.isValid()
+				return false;
 			}
 		}
-		return false;
+		return true;
 	}
 	
 	public static boolean validarFichero(Validable fichero) {
